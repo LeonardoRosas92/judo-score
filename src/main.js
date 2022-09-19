fa738e46cf77ea88ddccbfa2d4bdbfcbb346b843
Leonardo Rosas <leonrosant92@gmail.com>
2022-09-19 10:42:31 -0500

    :sparkles: Adding fullcreen

--- src/main.js
+++ src/main.js
@@ -3,4 +3,5 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/style.css'
-createApp(App).use(store).use(router).mount('#app')
+import VueFullscreen from 'vue-fullscreen'
+createApp(App).use(store).use(router).use(VueFullscreen).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="h-screen w-screen bg-gray-400">
-    <NavBar />
-    <MainContent />
-  </div>
+  <fullscreen v-model="fullscreen">
+    <div class="h-screen w-screen bg-gray-400 relative">
+      <button @click="toggle" class="absolute top-2 left-0 lg:top-5" >
+        <span class="material-icons fullscreen">fullscreen</span>
+      </button>
+      <NavBar />
+      <MainContent />
+      
+    </div>
+  </fullscreen>
 </template>
 
 <script>
@@ -11,5 +17,24 @@ import MainContent from "../components/MainContent.vue";
 export default {
   name: "HomeView",
   components: { NavBar, MainContent },
+  data() {
+    return {
+      fullscreen: false
+    }
+  },
+  methods: {
+    toggle () {
+      this.fullscreen = !this.fullscreen
+    },
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+/* .fullscreen{
+  position: absolute;
+  z-index: 10;
+  top: 0;
+  left: 0;
+} */
+</style>
