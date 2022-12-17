1addc94345f8c6ed5a4820d97227a18294149095
Leonardo Rosas <leonrosant92@gmail.com>
2022-12-16 21:34:29 -0600

    :sparkles: Adding decreases and increase seconds manually

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -29,6 +29,10 @@
         <TimerComponent title="Tiempo" isTimer class="order-2 md:col-span-6 md:order-2 " @disableSelect="disableSelect"/>
         <TimerComponent title="Osaekomi" isOsaekomi isAzul class="order-3 md:col-span-3 md:order-3"/>
     </div>
+    <div>
+        <button type="button" class="py-2 px-3 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" @click="this.incrementTimer()">+</button>
+        <button type="button" class="py-2 px-3 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" @click="this.decrementTimer">-</button>
+    </div>
   </main>
 </template>
 
@@ -48,7 +52,7 @@ export default {
         }
     },
     methods:{
-        ...mapActions(['setTimempoSeleccionado']),
+        ...mapActions(['setTimempoSeleccionado','decrementTimer','incrementTimer']),
         disableSelect(){
             this.isDisable = !this.isDisable;
         }
